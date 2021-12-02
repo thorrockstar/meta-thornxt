a7f4fa354e59983c3cec2c2653598cfb838f8478
thorrockstar <postmaster@overthehill.de>
2021-12-02 13:55:44 +0100

    Pushed master to dunfell level.

--- recipes-devtools/qemu/qemu-native_3.%.bbappend
+++
@@ -1,6 +0,0 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/patches:"
-
-SRC_URI += " \
-    file://0011-linux-user-remove-host-stime-syscall.patch \
-"
-
--- sam-ba/thor_linux_nandflash.tcl
+++ sam-ba/thor_linux_nandflash.tcl
@@ -2,7 +2,7 @@
 set bootstrapFile       "sama5d3_xplained-nandflashboot-uboot.bin"
 set ubootFile           "u-boot-sama5d3-xplained.bin"
 set kernelFile          "zImage-sama5d3_xplained.bin"
-set rootfsFile          "atmel-xplained-demo-image-sama5d3_xplained.ubi"
+set rootfsFile          "thor_sama5d3_xplained.ubi"
 
 ## board variant
 set boardFamily "at91-sama5d3"
